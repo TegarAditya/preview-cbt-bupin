07f2cb6b3caf587b0b7213d149532dc1495f72f7
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-16 11:52:46 +0700

    add default layout and index page for Nuxt.js application with Tailwind CSS

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="container w-screen min-h-[100dvh] flex flex-col justify-center items-center"
+  >
+    <div class="flex flex-col gap-3 mx-5">
+      <h1 class="text-2xl font-semibold font-space_grotesk text-center">
+        Selamat Datang, Nuxt.js!
+      </h1>
+      <div class="border border-gray-300 rounded-md p-4">
+        <p class="text-sm text-gray-500">
+          Ini adalah contoh aplikasi Nuxt.js dengan Tailwind CSS
+        </p>
+      </div>
+      <div class="flex justify-center">
+        <a href="https://play.google.com/store/apps/details?id=com.mmj.qr_scan&hl=id">
+          <span class="text-sm text-blue-500 underline underline-offset-2">Unduh Bupin Scanner</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
