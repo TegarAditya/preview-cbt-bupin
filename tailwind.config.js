d847261a342d2cfa50b17892b89ecb92c81c021e
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-16 11:52:24 +0700

    initialize Nuxt.js application with TypeScript, Tailwind CSS, and basic configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Quicksand', 'sans-serif'],
+        space_grotesk: ["'Space Grotesk'", 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
