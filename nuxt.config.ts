7f8b333a31f33b0aefd6a6ccc4a234a0a5db9e3a
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-21 13:54:50 +0700

    chore: add nuxt-icon module to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image', 'nuxt-icon']
 })
