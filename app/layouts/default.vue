e719e0a76578e50130667534404c3be23b0fe86f
Tegar Aditya <tegaraditya53@gmail.com>
2026-01-20 20:49:06 +0700

    feat: update SEO metadata in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script lang="ts" setup>
-
+useSeoMeta({
+  title: 'Preview CBT Bupin',
+  ogTitle: 'Preview CBT Bupin',
+  description: 'Aplikasi preview soal CBT dari Bupin untuk SD, SMP, dan SMA.',
+  ogDescription: 'Aplikasi preview soal CBT dari Bupin untuk SD, SMP, dan SMA.',
+  charset: 'utf-8',
+  viewport: 'width=device-width, initial-scale=1',
+})
 </script>
 
 <style>
