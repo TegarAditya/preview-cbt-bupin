f98a7b982875c6804853a653d097ad6a2d2c3347
Tegar Aditya <tegaraditya53@gmail.com>
2026-01-20 20:48:43 +0700

    feat: add devtoolsJson plugin and update app configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 import tailwindcss from '@tailwindcss/vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: { lang: 'id' },
+    }
+  },
+  appConfig: {
+    metaURL: process.env.META_BASE_URL || 'https://buku.bupin.id',
+    soalURL: process.env.SOAL_BASE_URL || 'https://cbt.api.bupin.id',
+  },
   css: ['~/assets/css/main.css'],
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), devtoolsJson()],
   },
   modules: ['@vueuse/nuxt', '@nuxt/icon'],
 })
